f2d4095c9e06db473e07e00cadd5c63c38de3363
Yuji Matusmoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2020-09-24 18:10:40 +0700

    Material-ui settings

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
 import { NextPage } from 'next';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
-const Index: NextPage = () => <div>index</div>;
+const Index: NextPage = () => (
+  <div>
+    <Typography variant="h1">Next.js example</Typography>
+    <Button color="primary">aaaa</Button>
+  </div>
+);
 
 export default Index;
