14e2122d729b835dc4a68ce4eadb8e95ab0d925d
Yuji Matusmoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2020-09-25 16:26:00 +0700

    Config material-ui theme

--- src/utility/theme.ts
+++ src/utility/theme.ts
@@ -1,5 +1,46 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import Rubik from '../../public/fonts/Rubik-Regular.ttf';
 
-const theme = createMuiTheme();
+const rubik = {
+  fontFamily: 'Rubik',
+  fontStyle: 'normal',
+  fontWeight: 400,
+  src: `
+    local('Rubik'),
+    local('Rubik-Regular'),
+    url(${Rubik}) format('ttf')
+  `,
+};
+
+const palette = {
+  primary: {
+    main: '#0336ff',
+  },
+  secondary: {
+    main: '#ff0266',
+  },
+  // error: {},
+  // warning: {},
+  // info: {},
+  // success: {},
+};
+
+const typography = {
+  fontFamily: 'Rubik',
+};
+
+const overrides = {
+  MuiCssBaseline: {
+    '@global': {
+      '@font-face': [rubik],
+    },
+  },
+};
+
+const theme = createMuiTheme({
+  palette,
+  typography,
+  overrides,
+});
 
 export default theme;
