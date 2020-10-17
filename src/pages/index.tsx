31fc3a6d3ea0791904b6f8b5e2729cdc9b9307df
Yuji Matusmoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2020-10-17 17:36:38 +0700

    Copy markdowns from my old blog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from 'next';
 import { Stack } from '@chakra-ui/core';
+import { Frontmatter } from 'types/models/post';
 import MainLayout from 'components/layouts';
 import BlogPost from 'components/molecules/BlogPost';
 
@@ -9,10 +10,16 @@ import BlogPost from 'components/molecules/BlogPost';
 import { frontMatter as blogPosts } from './blogs/*.mdx';
 
 const Index: NextPage = () => {
+  const sortedBlogPosts = blogPosts.sort((a: Frontmatter, b: Frontmatter) => {
+    return (
+      new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime()
+    );
+  });
+
   return (
     <MainLayout>
       <Stack spacing={4}>
-        {blogPosts.map((post) => (
+        {sortedBlogPosts.map((post: Frontmatter) => (
           <BlogPost post={post} key={post.title} />
         ))}
       </Stack>
