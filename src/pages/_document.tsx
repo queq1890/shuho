039e1d5d40a78de7d1c06c68a886713163cd84a8
Yuji Matusmoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2020-10-18 00:16:58 +0700

    Add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,9 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="shortcut icon" href="/static/favicons/favicon.ico" />
+        </Head>
         <body>
           <Main />
           <NextScript />
