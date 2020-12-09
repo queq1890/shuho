d5b49154d81711f66d2edcab351514b2738eaba9
Yuji Matsumoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2020-12-09 17:42:00 +0700

    Remove chakra's peer dependencies

--- src/components/molecules/BlogPostTitle/index.tsx
+++ src/components/molecules/BlogPostTitle/index.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react';
 import { format } from 'date-fns';
-import _Image from 'next/image';
-import styled from '@emotion/styled';
+import Image from 'next/image';
 import H1 from 'components/atoms/H1';
 
 import styles from './BlogPostTitle.module.scss';
@@ -17,7 +16,7 @@ const BlogPostTitle: FC<Props> = ({ title, publishedAt }) => {
       <H1 id={title}>{title}</H1>
       <div className="flex justify-between items-start md:items-center flex-col md:flex-row mt-2 mb-4 w-full">
         <div className="flex items-center">
-          <div className="mr-4">
+          <div className={`mr-4 ${styles.img}`}>
             <Image src="/static/images/avatar.jpeg" width={24} height={24} />
           </div>
           <p className="text-sm">
@@ -29,9 +28,5 @@ const BlogPostTitle: FC<Props> = ({ title, publishedAt }) => {
     </div>
   );
 };
-
-const Image = styled(_Image)`
-  border-radius: 9999px;
-`;
 
 export default BlogPostTitle;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,10 @@
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { MDXProvider } from '@mdx-js/react';
-import { Global, css } from '@emotion/react';
 import MDXComponents from 'components/molecules/MDXComponents';
 import 'tailwindcss/tailwind.css';
-import 'utility/prism.css';
+import 'styles/prism.scss';
+import 'styles/global.scss';
 
 const MyApp = (props: AppProps) => {
   const { Component, pageProps } = props;
@@ -18,27 +18,6 @@ const MyApp = (props: AppProps) => {
         />
       </Head>
 
-      <Global
-        styles={css`
-          @font-face {
-            font-family: 'Rubik';
-            src: url('/fonts/Rubik-Regular.ttf');
-            font-display: swap;
-          }
-
-          html {
-            min-width: 300px;
-            scroll-behavior: smooth;
-          }
-
-          #__next {
-            display: flex;
-            flex-direction: column;
-            min-height: 100vh;
-            background: white;
-          }
-        `}
-      />
       <MDXProvider components={MDXComponents}>
         <Component {...pageProps} />
       </MDXProvider>
