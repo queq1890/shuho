953db7d9ec533da3ff98ebc45dd90b03a445ee13
Yuji Matsumoto (queq1890) <yuji.matsumoto@sun-asterisk.com>
2020-12-25 17:27:44 +0700

    Add Vulf font config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      'vulf-mono': ['Vulf Mono'],
+      'vulf-sans': ['Vulf Sans'],
+    },
     extend: {
       colors: {
         primary: {
